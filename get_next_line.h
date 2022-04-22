089aa0fb2ee95933d805f6d0b2d945c6dbbb52dc
alexHonore <98293124+alexHonore@users.noreply.github.com>
2022-04-22 14:16:35 -0400

    Lets forget linked lists for know

---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anshimiy <anshimiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 14:13:04 by anshimiy          #+#    #+#             */
+/*   Updated: 2022/04/22 14:14:55 by anshimiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+char	*get_line(char **line, char **s_buff);
+int		read_file(int fd, char **buffer, char **s_buff, char **line);
+void	free_ptr(char *ptr);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+#endif
